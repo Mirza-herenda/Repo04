7482e17149be0d8e3785e7f93fab748ee722a992
Mirza <herendamirza87@gmail.com>
2022-03-31 16:27:36 +0200

    my new task :scientific notation

---
+++ ex04/src/index.js
@@ -0,0 +1,16 @@
+function main() 
+{
+ 
+  //only change below this line
+  var myDecimal=8;
+  var myDecimalE=254e-5;
+ 
+  //only change code above this line
+  return{myDecimal,myDecimalE};
+
+
+
+
+}
+console.log(main());
+module.exports=main;
