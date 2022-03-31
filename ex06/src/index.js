3bbb0909b0141be8ecb50df5d41c5e2c06e011f5
Mirza <herendamirza87@gmail.com>
2022-03-31 16:56:14 +0200

    new task:davide two numbers

---
+++ ex06/src/index.js
@@ -0,0 +1,21 @@
+function main(numOne,numTwo) 
+{
+ 
+  //only change below this line
+  var myQuotient;
+  var numOne;
+  var numTwo;
+  product=numOne/numTwo;
+ 
+  //only change code above this line
+  return product;
+  
+
+
+
+
+}
+console.log(main(5.0,2.0));
+console.log(main(16.5,5.5));
+console.log(main(102.0,25.5));
+module.exports=main;
